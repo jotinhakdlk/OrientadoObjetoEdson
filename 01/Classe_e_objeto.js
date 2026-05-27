a3d9e5eb72b108da4aaa8f024374b8bf80e84cbb
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-05-27 11:53:09 -0300

    Add files via upload

--- 01/Classe_e_objeto.js
+++ 01/Classe_e_objeto.js
@@ -10,5 +10,11 @@ function criarHeroi(){
     const nomeDigitado = document.getElementById('nomeInput').value;
     const classeSelecionada = document.getElementById('classeSelect').value;
     // Criando o objeto com os valores dos campos
-    const novoHeroi = new heroi(nomeDigitado, classeSelecionada)   
+    const novoHeroi = new heroi(nomeDigitado, classeSelecionada);
+
+    document.getElementById('resultado').style.display = "block";
+    document.getElementById('dadosHeroi').innerHTML = `
+    Nome: <strong>${novoHeroi.nome}</strong><br>
+    Classe: <strong>${novoHeroi.classe}</strong>
+    `;
 }
--- 01/exercício/Classe_e_objeto.js
+++ 01/exercício/Classe_e_objeto.js
@@ -6,3 +6,27 @@ Modifique a classe Heroi para que o construtor também receba e armazene esse n�
  No JavaScript da função, capture esse novo input e garanta que o nível seja exibido 
  no painel de resultado acessando a propriedade do objeto."
 */ 
+
+class heroi{
+    constructor(nome, classe, nivel){
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+    };
+
+};
+function criarHeroi(){
+    // Capturando o que o usuário digitou/selecionou na tela
+    const nomeDigitado = document.getElementById('nomeInput').value;
+    const classeSelecionada = document.getElementById('classeSelect').value;
+    const nivelHeroi = document.getElementById('nivelInput').value;
+    // Criando o objeto com os valores dos campos
+    const novoHeroi = new heroi(nomeDigitado, classeSelecionada, nivelHeroi);
+
+    document.getElementById('resultado').style.display = "block";
+    document.getElementById('dadosHeroi').innerHTML = `
+    Nome: <strong>${novoHeroi.nome}</strong><br>
+    Classe: <strong>${novoHeroi.classe}</strong><br>
+    Nivel: <strong>${novoHeroi.nivel}</strong>
+    `;
+}
